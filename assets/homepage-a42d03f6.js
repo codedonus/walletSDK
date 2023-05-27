d87f9e7459c281b5115efe57d20b76b99e9e45e1
codedonus <89918742+codedonus@users.noreply.github.com>
2023-05-28 01:30:16 +0800

    Add files via upload

---
+++ assets/homepage-a42d03f6.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-84472bf6.js";const s=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"主页"}),e.jsx("a",{href:"/profile",children:"个人主页"})]});export{s as default};
---
+++ assets/profile-692db8a6.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-84472bf6.js";const e=()=>r.jsx("h1",{children:"个人详情页"});export{e as default};
